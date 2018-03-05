85b8567f1d7254cba9bb13348932c97d5a427997
navidsarvian <navidsarv@gmail.com>
2018-03-04 22:56:57 -0800

    final

---
+++ app/data/friends.js
@@ -0,0 +1,18 @@
+//list of intial friends
+var friends = [
+	{
+		"name": "Kobe Bryant",
+		"photo": "https://espnfivethirtyeight.files.wordpress.com/2015/11/gettyimages-104445268.jpg?w=1892",
+		"scores": [1,1,1,1,1,1,1,1,1,1]
+		//10
+	},
+	{
+		"name": "Lebron James",
+		"photo": "https://pbs.twimg.com/media/Clf-NPOWIAAzvVj.jpg",
+		"scores": [5,5,5,5,5,5,5,5,5,5]
+		//55
+	}
+];
+
+//send back to server
+module.exports = friends;
